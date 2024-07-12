c6a8b56503e03516d49af56d6dfd3ac3fc10e1c9
thiagobrito <thiago7brito@gmail.com>
2024-07-12 17:29:32 -0300

    cargo

---
+++ target/debug/deps/rust_graduation_alura.d
@@ -0,0 +1,5 @@
+F:\Alura\Rust\rust-graduation-alura\target\debug\deps\rust_graduation_alura.exe: src\main.rs
+
+F:\Alura\Rust\rust-graduation-alura\target\debug\deps\rust_graduation_alura.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/rust-graduation-alura.d
@@ -0,0 +1,1 @@
+F:\Alura\Rust\rust-graduation-alura\target\debug\rust-graduation-alura.exe: F:\Alura\Rust\rust-graduation-alura\src\main.rs
